c862b8c6fadf50ac323d56f3ba5a23cd28bbaa8f
Ayoub <ayoubzoubeidi@gmail.com>
2022-09-16 17:10:58 +0100

    Initial commit

---
+++ src/main/java/com/maz/learnhexagonalarchitecture/LearnHexagonalArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.maz.learnhexagonalarchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnHexagonalArchitectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnHexagonalArchitectureApplication.class, args);
+    }
+
+}
